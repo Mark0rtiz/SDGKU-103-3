1526481db167d1883d3071095a6c8c129754a598
Mark0rtiz <106716676+Mark0rtiz@users.noreply.github.com>
2022-06-27 19:51:05 -0700

    Add files via upload

--- SDGKU JS Session 3/calculator.js
+++ SDGKU JS Session 3/calculator.js
@@ -4,11 +4,11 @@ function calculator(){
     let num1= Number(prompt("Enter a number"));
     let num2= Number(prompt("Enter a second number"));
     //do all four operations
-    document.write(`<p>Addition result: ${num1 + num2}</p>`);
-    document.write(`<p>Subtraction result: ${num1 - num2}</p>`);
-    document.write(`<p>Multiplication result: ${num1 * num2}</p>`);
-    document.write(`<p>Division result: ${num1 / num2}</p>`);
+    let result= `<p> ${num1 + num2}</p><p> ${num1 - num2}</p><p> ${num1 * num2}</p><p> ${num1 / num2}</p>`;
+
+
+    document.getElementById("results").innerHTML = result;
     //display the results of all four operations in the results area of HTML
 }
 
-document.getElementById("results").innerHTML = calculator();
+
